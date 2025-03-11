07c607e1f57672867a46f916989f0f43099987ab
Tyler Davenport <tydavenport0415@gmail.com>
2025-03-11 11:08:13 -0500

    need to set MVP branch before continuing

---
+++ src/components/forms/ProfileForm.js
@@ -0,0 +1,21 @@
+// 'use client';
+
+// import {useState} from "react";
+// import { useRouter } from "next/router";
+// import { useAuth } from "../../utils/context/authContext";
+
+// const initialState = {
+// bio: "",
+// contact: "",
+// displayName: "",
+// payment: "",
+// pfp: "",
+//   };
+
+//   function ProfileForm() {
+//     const [formInput, setFormInput] = useState(initialState);
+//     const router = useRouter();
+//     const { user } = useAuth();
+//   }
+
+//   export default ProfileForm;
